9c3a07698587f055eab9c418000e52e17b884f27
TheSalimi <amirhosein.srudsari@gmail.com>
2023-07-31 00:39:35 +0430

    user service interface

---
+++ mahakBackend.Domain/Iservice/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using mahakBackend.Core.Domain.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mahakBackend.Core.Domain.Iservice
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserEntity>> GetAllUsers();
+        Task<UserEntity> GetById(int id);
+        Task Add(UserEntity user);
+        Task Update(UserEntity user);
+        Task Delete(int id);
+    }
+}
