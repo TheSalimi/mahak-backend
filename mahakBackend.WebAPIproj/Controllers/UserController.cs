4e9e1bca4914511d852d908ec00dcef5d59d594e
TheSalimi <amirhosein.srudsari@gmail.com>
2023-07-31 18:01:41 +0430

    make first api
    post api for user

---
+++ mahakBackend.WebAPIproj/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using mahakBackend.Core.Domain.entities;
+using mahakBackend.Core.Domain.Irepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mahakBackend.WebAPIproj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;   
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] UserEntity user)
+        {
+            if(user == null)
+            {
+                return BadRequest("User data is null");
+            }
+            _userRepository.Add(user);
+            return Ok("User added successfully.");
+        }
+    }
+}
--- mahakBackend.WebAPIproj/Program.cs
+++ mahakBackend.WebAPIproj/Program.cs
@@ -1,9 +1,20 @@
+using mahakBackend.Core.Application.services;
+using mahakBackend.Core.Domain.Irepository;
+using mahakBackend.Core.Domain.Iservice;
 using mahakBackend.Infrastructure.Data;
+using mahakBackend.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IUser_RoleRepository, User_RoleRepository>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IUser_RoleService, User_RoleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
