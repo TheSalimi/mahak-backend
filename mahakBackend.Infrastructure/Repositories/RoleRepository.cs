0fa088937c7f18be62582979251eac347b0c9015
TheSalimi <amirhosein.srudsari@gmail.com>
2023-07-31 12:26:54 +0430

    Create RoleRepository.cs

---
+++ mahakBackend.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,40 @@
+﻿using mahakBackend.Core.Domain.entities;
+using mahakBackend.Core.Domain.Irepository;
+using mahakBackend.Infrastructure.Data;
+
+namespace mahakBackend.Infrastructure.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly MahakDbContext _DbContext;
+        public RoleRepository(MahakDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public void Add(RoleEntity role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RoleEntity> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoleEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(RoleEntity role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
