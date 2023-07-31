91aa3753b80096b60f485e85ef4b2c429fdf73e5
TheSalimi <amirhosein.srudsari@gmail.com>
2023-07-31 12:27:02 +0430

    Create User_RoleService.cs

---
+++ mahakBackend.Application/services/User_RoleService.cs
@@ -0,0 +1,40 @@
+﻿using mahakBackend.Core.Domain.entities;
+using mahakBackend.Core.Domain.Irepository;
+using mahakBackend.Core.Domain.Iservice;
+
+namespace mahakBackend.Core.Application.services
+{
+    public class User_RoleService : IUser_RoleService
+    {
+        IUser_RoleRepository _user_roleRepository { get; set; }
+        public User_RoleService(IUser_RoleRepository user_RoleRepository) 
+        { 
+            _user_roleRepository = user_RoleRepository;
+        }
+
+        public void Add(User_RoleEntity user_role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RoleEntity> GetRolesByUserCode(long UserCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserEntity> GetUsersByRoleId(int RoleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User_RoleEntity user_role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
