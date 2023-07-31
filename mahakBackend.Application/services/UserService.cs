f9a023b9d890bcc23fcfabeb3567ef9b3514998b
TheSalimi <amirhosein.srudsari@gmail.com>
2023-07-31 12:27:06 +0430

    Create UserService.cs

---
+++ mahakBackend.Application/services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using mahakBackend.Core.Domain.entities;
+using mahakBackend.Core.Domain.Irepository;
+using mahakBackend.Core.Domain.Iservice;
+
+namespace mahakBackend.Core.Application.services
+{
+    public class UserService: IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository) 
+        {
+            _userRepository = userRepository;
+        }
+
+        public void Add(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserEntity> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
