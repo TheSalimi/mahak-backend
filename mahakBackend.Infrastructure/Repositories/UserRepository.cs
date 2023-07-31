7d1978beadf9a568abf93aa1220a43b1521768d6
TheSalimi <amirhosein.srudsari@gmail.com>
2023-07-31 12:26:47 +0430

    Create UserRepository.cs

---
+++ mahakBackend.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using mahakBackend.Core.Domain.entities;
+using mahakBackend.Core.Domain.Irepository;
+using mahakBackend.Infrastructure.Data;
+
+namespace mahakBackend.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MahakDbContext _DbContext;
+        public UserRepository(MahakDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public void Add(UserEntity user)
+        {
+            _DbContext.Users.Add(user);
+            _DbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var user = _DbContext.Users.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                _DbContext.Users.Remove(user);
+                _DbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<UserEntity> GetAllUsers()
+        {
+            return _DbContext.Users.ToList();   
+        }
+
+        public UserEntity GetById(int id)
+        {
+            return _DbContext.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public void Update(UserEntity user)
+        {
+            _DbContext.Users.Update(user);
+            _DbContext.SaveChanges();
+        }
+    }
+}
