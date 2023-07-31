81f001c4f00999135366e9aa4a5656df030c6879
TheSalimi <amirhosein.srudsari@gmail.com>
2023-07-31 12:26:58 +0430

    Create RoleService.cs

---
+++ mahakBackend.Application/services/RoleService.cs
@@ -0,0 +1,39 @@
+﻿using mahakBackend.Core.Domain.entities;
+using mahakBackend.Core.Domain.Irepository;
+using mahakBackend.Core.Domain.Iservice;
+
+namespace mahakBackend.Core.Application.services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IRoleRepository _roleRepository;
+        public RoleService(IRoleRepository RoleRepository)
+        { 
+            _roleRepository = RoleRepository;
+        }
+        public void Add(RoleEntity role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RoleEntity> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoleEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(RoleEntity role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
