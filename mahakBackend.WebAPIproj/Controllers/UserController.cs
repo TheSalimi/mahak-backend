d426355df57a66bddbc2f67d075fea200aa0555d
TheSalimi <amirhosein.srudsari@gmail.com>
2023-08-09 15:58:18 +0430

    add user from person API

--- mahakBackend.WebAPIproj/Controllers/UserController.cs
+++ mahakBackend.WebAPIproj/Controllers/UserController.cs
@@ -50,6 +50,25 @@ namespace mahakBackend.WebAPIproj.Controllers
             return Ok("User updated successfully.");
         }
 
+        [HttpPut("AddToUsers")]
+        public IActionResult AddedToUsers(int id,string description)
+        {
+            var user = _userService.GetById(id);
+            if (user == null)
+            {
+                return BadRequest("There is no user with this ID!");
+            }
+
+            user.Description = description;
+            user.isAdded = true;
+            user.JoinDate = DateTime.Now;
+            user.LastActivity = DateTime.Now;
+            user.UserCode = (user.Id+100000).ToString();
+
+            _userService.Update(user);
+            return Ok("new user is added successful");
+        }
+
         [HttpDelete]
         public IActionResult DeleteUser(int id)
         {
